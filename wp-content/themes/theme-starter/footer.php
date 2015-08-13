2d9dc06be30a641219d542ccece5841595165328
Cameron Helkuik <cameronhelkuik@gmail.com>
2015-08-13 09:49:56 -0500

    Added footer

--- wp-content/themes/theme-starter/footer.php
+++ wp-content/themes/theme-starter/footer.php
@@ -4,6 +4,17 @@
  */
 
 ?>
+    <footer>
+      <div class="footer_logo" alt="New Grit Soap Co."></div>
+      
+      <ul>
+        <li><a href="https://www.facebook.com/Newgritsoapco"><img src="assets/fb.png"></a></li>
+        <li><a href="https://instagram.com/newgritsoapco"><img src="assets/instagram.png"></a></li>
+        <li><a href="https://www.pinterest.com/anodyneb/new-grit-soap-co/"><img src="assets/pinterest.png"></a></li>
+      </ul>
+    </footer>
+          
+    <script type="text/javascript" src="global.js"></script>
 
   <?php wp_footer(); ?>
 
