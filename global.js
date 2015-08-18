f40ad340d6e2756ed849afc7cbce39e40b4a7433
Cameron Helkuik <cameronhelkuik@gmail.com>
2015-08-18 12:48:53 -0500

    Commented out js that is unnecessary due to plugin

--- global.js
+++ global.js
@@ -30,32 +30,32 @@ function shrink_header(){
 }
 
 // Sliding images----------------------------
-(function(){
- 
-  var counter = 0,
-  $items = document.querySelectorAll('.jumbotron .slide'),
-  numItems = $items.length;
- 
-  var showCurrent = function(){
-    var itemToShow = Math.abs(counter%numItems);
- 
-    [].forEach.call( $items, function(el){
-      el.classList.remove('show');
-    });
- 
-    $items[itemToShow].classList.add('show');
-  };
- 
-  document.querySelector('.next').addEventListener('click', function() {
-    counter++;
-    showCurrent();
-  }, false);
- 
-  document.querySelector('.prev').addEventListener('click', function() {
-    counter--;
-    showCurrent();
-  }, false);
- 
-})();
+// (function(){
+//
+//   var counter = 0,
+//   $items = document.querySelectorAll('.jumbotron .slide'),
+//   numItems = $items.length;
+//
+//   var showCurrent = function(){
+//     var itemToShow = Math.abs(counter%numItems);
+//
+//     [].forEach.call( $items, function(el){
+//       el.classList.remove('show');
+//     });
+//
+//     $items[itemToShow].classList.add('show');
+//   };
+//
+//   document.querySelector('.next').addEventListener('click', function() {
+//     counter++;
+//     showCurrent();
+//   }, false);
+//
+//   document.querySelector('.prev').addEventListener('click', function() {
+//     counter--;
+//     showCurrent();
+//   }, false);
+//
+// })();
 
 
