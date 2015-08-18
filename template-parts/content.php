217427ce0fba05b66221a2a85cb473bded9d5ffa
Cameron Helkuik <cameronhelkuik@gmail.com>
2015-08-18 12:43:16 -0500

    Not working with flywheel, fixing minor things to see if it fixes it

--- template-parts/content.php
+++ template-parts/content.php
@@ -8,7 +8,8 @@
 <div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
   <div class="entry-header">
   </div><!-- .entry-header -->
-      
+    
+  <div class="entry-content"> 
     <?php the_content(); ?>
 
     <?php wp_link_pages(); ?>
